4f24caeae07a5fe0fcaea9b3682dd0df4a6942fb
dongsuk <scienceyds@naver.com>
2023-05-18 16:11:30 +0900

    6장 02 성적이 낮은 순서로 학생 출력하기

---
+++ Chapter06 정렬/02 성적이 낮은 순서로 학생 출력하기.py
@@ -0,0 +1,11 @@
+n=int(input())
+lst=[[] for i in range(101)]
+print(lst)
+for i in range(n):
+    data=input().split()
+    name=data[0]
+    score=int(data[1])
+    lst[score].append(name)
+for i in range(101):
+    for j in range(len(lst[i])):
+        print(lst[i][j],end=" ")
